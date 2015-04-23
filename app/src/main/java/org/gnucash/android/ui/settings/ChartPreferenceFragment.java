d88c6c88c3e66486e9bc822be18cda0feb5f3066
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2015-04-23 20:54:19 +0300

    Added chart preferences

---
+++ app/src/main/java/org/gnucash/android/ui/settings/ChartPreferenceFragment.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2015 Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gnucash.android.ui.settings;
+
+import android.annotation.TargetApi;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockPreferenceActivity;
+
+import org.gnucash.android.R;
+import org.gnucash.android.db.AccountsDbAdapter;
+import org.gnucash.android.model.Money;
+
+import java.util.Currency;
+import java.util.List;
+
+/**
+ * Fragment for charts configuring
+ * @author Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
+ */
+@TargetApi(11)
+public class ChartPreferenceFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.fragment_chart_preferences);
+
+        ActionBar actionBar = ((SherlockPreferenceActivity) getActivity()).getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Chart Prefs");
+
+        List<Currency> currencyList = AccountsDbAdapter.getInstance().getCurrencies();
+        int size = currencyList.size();
+        String[] currencyCodes = new String[size];
+        for (Currency currency : currencyList) {
+            currencyCodes[--size] = currency.getCurrencyCode();
+        }
+
+        ListPreference pref = (ListPreference) findPreference(getString(R.string.key_chart_currency));
+        pref.setEntries(currencyCodes);
+        pref.setEntryValues(currencyCodes);
+//        android:entries="@array/currency_names"
+//        android:entryValues="@array/key_currency_codes"
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String keyChartCurrency = getString(R.string.key_chart_currency);
+        Preference pref = findPreference(keyChartCurrency);
+        String chartCurrency = sharedPreferences.getString(keyChartCurrency, null);
+        if (chartCurrency != null && !chartCurrency.trim().isEmpty()) {
+            pref.setSummary(chartCurrency);
+        } else {
+            pref.setSummary(sharedPreferences.getString(getString(R.string.key_default_currency), Money.DEFAULT_CURRENCY_CODE));
+        }
+
+    }
+
+}
