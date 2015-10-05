badc4422f278df755c13740c91ff849a546c0e35
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2015-10-05 17:55:01 +0300

    Fixed extra repainting bar chart

--- app/src/main/java/org/gnucash/android/ui/report/BarChartFragment.java
+++ app/src/main/java/org/gnucash/android/ui/report/BarChartFragment.java
@@ -95,6 +95,8 @@ public class BarChartFragment extends Fragment implements OnChartValueSelectedLi
 
     private Currency mCurrency;
 
+    private AccountType mAccountType;
+
     private boolean mUseAccountColor = true;
     private boolean mTotalPercentageMode = true;
     private boolean mChartDataPresent = true;
@@ -139,6 +141,7 @@ public class BarChartFragment extends Fragment implements OnChartValueSelectedLi
         ReportsActivity reportsActivity = (ReportsActivity) getActivity();
         mReportStartTime = reportsActivity.getReportStartTime();
         mReportEndTime = reportsActivity.getReportEndTime();
+        mAccountType = reportsActivity.getAccountType();
 
         mChart.setOnChartValueSelectedListener(this);
         mChart.setDescription("");
@@ -152,24 +155,24 @@ public class BarChartFragment extends Fragment implements OnChartValueSelectedLi
         chartLegend.setPosition(Legend.LegendPosition.RIGHT_OF_CHART_INSIDE);
 //        chartLegend.setWordWrapEnabled(true); in MPAndroidChart 2.1.3 legend wrapping cause app crash
 
-        mChart.setData(getData(((ReportsActivity) getActivity()).getAccountType()));
+        mChart.setData(getData());
         displayChart();
     }
 
 
     /**
      * Returns a data object that represents a user data of the specified account types
-     * @param accountType account's type which will be displayed
      * @return a {@code BarData} instance that represents a user data
      */
-    private BarData getData(AccountType accountType) {
+    private BarData getData() {
         List<BarEntry> values = new ArrayList<>();
         List<String> labels = new ArrayList<>();
         List<Integer> colors = new ArrayList<>();
         Map<String, Integer> accountToColorMap = new LinkedHashMap<>();
         List<String> xValues = new ArrayList<>();
-        LocalDateTime tmpDate = new LocalDateTime(getStartDate(accountType).toDate().getTime());
-        int count = getDateDiff(new LocalDateTime(getStartDate(accountType).toDate().getTime()), new LocalDateTime(getEndDate(accountType).toDate().getTime()));
+        LocalDateTime tmpDate = new LocalDateTime(getStartDate(mAccountType).toDate().getTime());
+        int count = getDateDiff(new LocalDateTime(getStartDate(mAccountType).toDate().getTime()),
+                new LocalDateTime(getEndDate(mAccountType).toDate().getTime()));
         for (int i = 0; i <= count; i++) {
             long start = 0;
             long end = 0;
@@ -199,7 +202,7 @@ public class BarChartFragment extends Fragment implements OnChartValueSelectedLi
             }
             List<Float> stack = new ArrayList<>();
             for (Account account : mAccountsDbAdapter.getSimpleAccountList()) {
-                if (account.getAccountType() == accountType
+                if (account.getAccountType() == mAccountType
                         && !account.isPlaceholderAccount()
                         && account.getCurrency() == mCurrency) {
 
@@ -221,7 +224,7 @@ public class BarChartFragment extends Fragment implements OnChartValueSelectedLi
                         stack.add((float) balance);
                         labels.add(account.getName());
                         colors.add(accountToColorMap.get(account.getUID()));
-                        Log.d(TAG, accountType + tmpDate.toString(" MMMM yyyy ") + account.getName() + " = " + stack.get(stack.size() - 1));
+                        Log.d(TAG, mAccountType + tmpDate.toString(" MMMM yyyy ") + account.getName() + " = " + stack.get(stack.size() - 1));
                     }
                 }
             }
@@ -386,21 +389,24 @@ public class BarChartFragment extends Fragment implements OnChartValueSelectedLi
         mReportStartTime = start;
         mReportEndTime = end;
 
-        mChart.setData(getData(((ReportsActivity) getActivity()).getAccountType()));
+        mChart.setData(getData());
         displayChart();
     }
 
     @Override
     public void onGroupingUpdated(GroupInterval groupInterval) {
         mGroupInterval = groupInterval;
-        mChart.setData(getData(((ReportsActivity) getActivity()).getAccountType()));
+        mChart.setData(getData());
         displayChart();
     }
 
     @Override
     public void onAccountTypeUpdated(AccountType accountType) {
-        mChart.setData(getData(accountType));
-        displayChart();
+        if (mAccountType != accountType) {
+            mAccountType = accountType;
+            mChart.setData(getData());
+            displayChart();
+        }
     }
 
     @Override
