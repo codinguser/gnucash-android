4a34625499862ce17b29c02e26a3e3a8296037f0
Aleksey Ivanovski <alexei.ivanovski@gmail.com>
2016-04-11 23:54:36 +0300

    Fix bug that caused displaying identical fragments inside ViewPager in main screen.

--- app/src/main/java/org/gnucash/android/ui/account/AccountsListFragment.java
+++ app/src/main/java/org/gnucash/android/ui/account/AccountsListFragment.java
@@ -105,6 +105,11 @@ public class AccountsListFragment extends Fragment implements
      */
     protected static final String TAG = "AccountsListFragment";
 
+	/**
+	 * Tag to save {@link AccountsListFragment#mDisplayMode} to fragment state
+     */
+    private static final String STATE_DISPLAY_MODE = "mDisplayMode";
+
     /**
      * Database adapter for loading Account records from the database
      */
@@ -176,6 +181,9 @@ public class AccountsListFragment extends Fragment implements
         Bundle args = getArguments();
         if (args != null)
             mParentAccountUID = args.getString(UxArgument.PARENT_ACCOUNT_UID);
+
+        if (savedInstanceState != null)
+            mDisplayMode = (DisplayMode) savedInstanceState.getSerializable(STATE_DISPLAY_MODE);
     }
 
     @Override
@@ -293,6 +301,12 @@ public class AccountsListFragment extends Fragment implements
     @Override
     public void refresh() {
         getLoaderManager().restartLoader(0, null, this);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(STATE_DISPLAY_MODE, mDisplayMode);
     }
 
     /**
