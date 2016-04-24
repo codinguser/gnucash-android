codinguser/gnucash-android

commit 66cc5332e67e04cb6ce00c558237ea32a77bc627

Avoid losing precision when the user enters an exchange rate.
We were storing the rate as originAmount/convertedAmount, which was
limited to the precision of the currencies. Now we store directly the
entered rate.

Fixes https://github.com/codinguser/gnucash-android/issues/479

app/src/main/java/org/gnucash/android/model/Price.java
@@ -2,7 +2,11 @@ package org.gnucash.android.model;
 
 import org.gnucash.android.util.TimestampHelper;
 
+
+import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * Model for commodity prices
@@ -35,6 +39,19 @@ public class Price extends BaseModel {
         this.mCommodityUID = commodityUID;
         this.mCurrencyUID = currencyUID;
         mDate = TimestampHelper.getTimestampFromNow();
+    }
+
+    /**
+     * Create new instance with the GUIDs of the commodities and the specified exchange rate.
+     * @param commodity1UID GUID of the origin commodity
+     * @param commodity2UID GUID of the target commodity
+     * @param exchangeRate exchange rate between the commodities
+     */
+    public Price(String commodity1UID, String commodity2UID, BigDecimal exchangeRate) {
+        this(commodity1UID, commodity2UID);
+        // Store 0.1234 as 1234/10000
+        setValueNum(exchangeRate.unscaledValue().longValue());
+        setValueDenom(BigDecimal.ONE.scaleByPowerOfTen(exchangeRate.scale()).longValue());
     }
 
     public String getCommodityUID() {
@@ -122,5 +139,21 @@ public class Price extends BaseModel {
             mValueNum /= commonDivisor;
             mValueDenom /= commonDivisor;
         }
+    }
+
+    /**
+     * Returns the exchange rate as a string formatted with the default locale.
+     *
+     * <p>It will have up to 6 decimal places.
+     *
+     * <p>Example: "0.123456"
+     */
+    @Override
+    public String toString() {
+        BigDecimal numerator = new BigDecimal(mValueNum);
+        BigDecimal denominator = new BigDecimal(mValueDenom);
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getNumberInstance();
+        formatter.setMaximumFractionDigits(6);
+        return formatter.format(numerator.divide(denominator));
     }
 }

app/src/test/java/org/gnucash/android/test/unit/model/PriceTest.java
@@ -1,0 +1,54 @@
+/*
+ * Copyright (c) 2016 Àlex Magaz Graça <rivaldi8@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.gnucash.android.test.unit.model;
+
+import org.gnucash.android.model.Price;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class PriceTest {
+    @Test
+    public void creatingFromExchangeRate_ShouldGetPrecisionRight() {
+        Locale.setDefault(Locale.US);
+
+        String exchangeRateString = "0.123456";
+        BigDecimal exchangeRate = new BigDecimal(exchangeRateString);
+        Price price = new Price("commodity1UID", "commodity2UID", exchangeRate);
+        assertThat(price.toString()).isEqualTo(exchangeRateString);
+
+        // ensure we don't get more decimal places than needed (0.123000)
+        exchangeRateString = "0.123";
+        exchangeRate = new BigDecimal(exchangeRateString);
+        price = new Price("commodity1UID", "commodity2UID", exchangeRate);
+        assertThat(price.toString()).isEqualTo(exchangeRateString);
+    }
+
+    @Test
+    public void toString_shouldUseDefaultLocale() {
+        Locale.setDefault(Locale.GERMANY);
+
+        String exchangeRateString = "1.234";
+        BigDecimal exchangeRate = new BigDecimal(exchangeRateString);
+        Price price = new Price("commodity1UID", "commodity2UID", exchangeRate);
+        assertThat(price.toString()).isEqualTo("1,234");
+    }
+}

app/src/main/java/org/gnucash/android/ui/transaction/dialog/TransferFundsDialogFragment.java
@@ -116,17 +116,21 @@ public class TransferFundsDialogFragment extends DialogFragment {
         Commodity currencyCommodity = commoditiesDbAdapter.getCommodity(mTargetCurrency.getCurrencyCode());
         String currencyUID = currencyCommodity.getUID();
         PricesDbAdapter pricesDbAdapter = PricesDbAdapter.getInstance();
-        Pair<Long, Long> price = pricesDbAdapter.getPrice(commodityUID, currencyUID);
+        Pair<Long, Long> pricePair = pricesDbAdapter.getPrice(commodityUID, currencyUID);
 
-        if (price.first > 0 && price.second > 0) {
+        if (pricePair.first > 0 && pricePair.second > 0) {
             // a valid price exists
-            BigDecimal numerator = new BigDecimal(price.first);
-            BigDecimal denominator = new BigDecimal(price.second);
-            DecimalFormat formatter = (DecimalFormat) NumberFormat.getNumberInstance();
-            mExchangeRateInput.setText(formatter.format(numerator.divide(denominator, MathContext.DECIMAL32)));
+            Price price = new Price(commodityUID, currencyUID);
+            price.setValueNum(pricePair.first);
+            price.setValueDenom(pricePair.second);
+            mExchangeRateInput.setText(price.toString());
+
+            BigDecimal numerator = new BigDecimal(pricePair.first);
+            BigDecimal denominator = new BigDecimal(pricePair.second);
             // convertedAmount = mOriginAmount * numerator / denominator
             BigDecimal convertedAmount = mOriginAmount.asBigDecimal().multiply(numerator)
                 .divide(denominator, currencyCommodity.getSmallestFractionDigits(), BigDecimal.ROUND_HALF_EVEN);
+            DecimalFormat formatter = (DecimalFormat) NumberFormat.getNumberInstance();
             mConvertedAmountInput.setText(formatter.format(convertedAmount));
         }
 
@@ -194,17 +198,24 @@ public class TransferFundsDialogFragment extends DialogFragment {
      * Converts the currency amount with the given exchange rate and saves the price to the db
      */
     private void transferFunds() {
+        Price price = null;
+
+        CommoditiesDbAdapter commoditiesDbAdapter = CommoditiesDbAdapter.getInstance();
+        String originCommodityUID = commoditiesDbAdapter.getCommodityUID(mOriginAmount.getCurrency().getCurrencyCode());
+        String targetCommodityUID = commoditiesDbAdapter.getCommodityUID(mTargetCurrency.getCurrencyCode());
+
         if (mExchangeRateRadioButton.isChecked()){
+            BigDecimal rate;
             String exchangeRateString = mExchangeRateInput.getText().toString();
             DecimalFormat formatter = (DecimalFormat) NumberFormat.getNumberInstance();
             formatter.setParseBigDecimal(true);
-            BigDecimal rate;
             try {
                 rate = (BigDecimal) formatter.parse(exchangeRateString);
             } catch (ParseException e) {
                 mExchangeRateInputLayout.setError(getString(R.string.error_invalid_exchange_rate));
                 return;
             }
+            price = new Price(originCommodityUID, targetCommodityUID, rate);
             mConvertedAmount = mOriginAmount.multiply(rate);
         }
 
@@ -217,22 +228,20 @@ public class TransferFundsDialogFragment extends DialogFragment {
 
             BigDecimal amount = TransactionFormFragment.parseInputToDecimal(convertedAmount);
             mConvertedAmount = new Money(amount, Commodity.getInstance(mTargetCurrency.getCurrencyCode()));
-        }
 
-        if (mOnTransferFundsListener != null) {
-            PricesDbAdapter pricesDbAdapter = PricesDbAdapter.getInstance();
-            CommoditiesDbAdapter commoditiesDbAdapter = CommoditiesDbAdapter.getInstance();
-            Price price = new Price(commoditiesDbAdapter.getCommodityUID(mOriginAmount.getCurrency().getCurrencyCode()),
-                    commoditiesDbAdapter.getCommodityUID(mTargetCurrency.getCurrencyCode()));
-            price.setSource(Price.SOURCE_USER);
-            // fractions cannot be exacted represented by BigDecimal.
+            price = new Price(originCommodityUID, targetCommodityUID);
+            // fractions cannot be exactly represented by BigDecimal.
             price.setValueNum(mConvertedAmount.getNumerator() * mOriginAmount.getDenominator());
             price.setValueDenom(mOriginAmount.getNumerator() * mConvertedAmount.getDenominator());
-            price.reduce();
-            pricesDbAdapter.addRecord(price);
-
-            mOnTransferFundsListener.transferComplete(mConvertedAmount);
         }
+
+        price.setSource(Price.SOURCE_USER);
+        price.reduce();
+        PricesDbAdapter.getInstance().addRecord(price);
+
+        if (mOnTransferFundsListener != null)
+            mOnTransferFundsListener.transferComplete(mConvertedAmount);
+
         dismiss();
     }
 

commit fd7007ef21c50b1fb3b24d06ac63173b02832f5f

Avoid Price.toString() failing for certain values of numerator/denominator.
Fixes a regression introduced in 66cc533 (bugfix #479) in which the
numerator/denominator division fails with an ArithmeticException if no
MathContext is provided. BigDecimal tries to return an exact result,
which is not possible when rounding and precision aren't specified.

See BigDecimal javadoc:
http://docs.oracle.com/javase/7/docs/api/java/math/BigDecimal.html

Fixes Crashlitics issue #196
https://fabric.io/gnucash/android/apps/org.gnucash.android/issues/571b4a95ffcdc04250f98ba0

app/src/main/java/org/gnucash/android/model/Price.java
@@ -4,6 +4,7 @@ import org.gnucash.android.util.TimestampHelper;
 
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.sql.Timestamp;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -156,6 +157,6 @@ public class Price extends BaseModel {
         BigDecimal denominator = new BigDecimal(mValueDenom);
         DecimalFormat formatter = (DecimalFormat) NumberFormat.getNumberInstance();
         formatter.setMaximumFractionDigits(6);
-        return formatter.format(numerator.divide(denominator));
+        return formatter.format(numerator.divide(denominator, MathContext.DECIMAL32));
     }
 }

app/src/test/java/org/gnucash/android/test/unit/model/PriceTest.java
@@ -23,6 +23,7 @@ import java.math.BigDecimal;
 import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
 
 
 public class PriceTest {
@@ -50,6 +51,26 @@ public class PriceTest {
         BigDecimal exchangeRate = new BigDecimal(exchangeRateString);
         Price price = new Price("commodity1UID", "commodity2UID", exchangeRate);
         assertThat(price.toString()).isEqualTo("1,234");
+    }
+
+    /**
+     * BigDecimal throws an ArithmeticException if it can't represent exactly
+     * a result. This can happen with divisions like 1/3 if no precision and
+     * round mode is specified with a MathContext.
+     */
+    @Test
+    public void toString_shouldNotFailForInfinitelyLongDecimalExpansion() {
+        long numerator = 1;
+        long denominator = 3;
+        Price price = new Price();
+
+        price.setValueNum(numerator);
+        price.setValueDenom(denominator);
+        try {
+            price.toString();
+        } catch (ArithmeticException e) {
+            fail("The numerator/denominator division in Price.toString() should not fail.");
+        }
     }
 
     @Test