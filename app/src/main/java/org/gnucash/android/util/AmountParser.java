945fd36b6239a1fe01526d91797c37d36e700092
Àlex Magaz Graça <rivaldi8@gmail.com>
2016-05-23 19:42:57 +0200

    Extract TransferFundsDialogFragment.parseAmount into a new class AmountParser.
    This makes testing easier.

---
+++ app/src/main/java/org/gnucash/android/util/AmountParser.java
@@ -0,0 +1,32 @@
+package org.gnucash.android.util;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.ParsePosition;
+
+/**
+ * Parses amounts as String into BigDecimal.
+ */
+public class AmountParser {
+    /**
+     * Parses {@code amount} and returns it as a BigDecimal.
+     *
+     * @param amount String with the amount to parse.
+     * @return The amount parsed as a BigDecimal.
+     * @throws ParseException if the full string couldn't be parsed as an amount.
+     */
+    public static BigDecimal parse(String amount) throws ParseException {
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getNumberInstance();
+        formatter.setParseBigDecimal(true);
+        ParsePosition parsePosition = new ParsePosition(0);
+        BigDecimal parsedAmount = (BigDecimal) formatter.parse(amount, parsePosition);
+
+        // Ensure any mistyping by the user is caught instead of partially parsed
+        if (parsePosition.getIndex() < amount.length())
+            throw new ParseException("Parse error", parsePosition.getErrorIndex());
+
+        return parsedAmount;
+    }
+}
