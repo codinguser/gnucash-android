009d73d683735154b594ea659e489a702790c0a7
Àlex Magaz Graça <rivaldi8@gmail.com>
2016-05-26 20:23:25 +0200

    Avoid returning null when parsing an empty string as an amount.
    Parsing an empty string wasn't throwing a ParseException as expected, it
    was returning null. Now we ensure the exception is thrown.
    
    Fixes https://github.com/codinguser/gnucash-android/issues/505

--- app/src/main/java/org/gnucash/android/util/AmountParser.java
+++ app/src/main/java/org/gnucash/android/util/AmountParser.java
@@ -24,7 +24,7 @@ public class AmountParser {
         BigDecimal parsedAmount = (BigDecimal) formatter.parse(amount, parsePosition);
 
         // Ensure any mistyping by the user is caught instead of partially parsed
-        if (parsePosition.getIndex() < amount.length())
+        if ((parsedAmount == null) || (parsePosition.getIndex() < amount.length()))
             throw new ParseException("Parse error", parsePosition.getErrorIndex());
 
         return parsedAmount;
