ad478f69aa2eed37f8f98c66fa6fd5523f7c1d32
Ngewi Fet <ngewif@gmail.com>
2018-06-04 18:58:03 +0200

    Enable support for exporting transaction splits separately in CSV exports
    Modified transactions CSV exporter to produce similar CSV like GnuCash desktop - #756
    Localize transaction CSV headers

--- app/src/main/java/org/gnucash/android/export/csv/CsvWriter.java
+++ app/src/main/java/org/gnucash/android/export/csv/CsvWriter.java
@@ -17,6 +17,9 @@
 package org.gnucash.android.export.csv;
 
 
+import android.support.annotation.NonNull;
+
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.Writer;
 
@@ -24,25 +27,68 @@ import java.io.Writer;
  * Format data to be CSV-compatible
  *
  * @author Semyannikov Gleb <nightdevgame@gmail.com>
+ * @author Ngewi Fet <ngewif@gmail.com>
  */
-public class CsvWriter {
-    private Writer writer;
+public class CsvWriter extends BufferedWriter {
+    private String separator = ",";
 
     public CsvWriter(Writer writer){
-        this.writer = writer;
+        super(writer);
     }
 
-    public void write(String str) throws IOException {
-        if (str == null || str.length() < 1) {
-            return;
-        }
-
-        String head = str.substring(0, str.length() - 1);
-        char separator = str.charAt(str.length() - 1);
-        if (head.indexOf(separator) > -1) {
-            head = '"' + head + '"';
-        }
-
-        writer.write(head + separator);
+    public CsvWriter(Writer writer, String separator){
+        super(writer);
+        this.separator = separator;
     }
+
+    @Override
+    public void write(@NonNull String str) throws IOException {
+        this.write(str, 0, str.length());
+    }
+
+    /**
+     * Writes a CSV token and the separator to the underlying output stream.
+     *
+     * The token **MUST NOT** not contain the CSV separator. If the separator is found in the token, then
+     * the token will be escaped as specified by RFC 4180
+     * @param token Token to be written to file
+     * @throws IOException if the token could not be written to the underlying stream
+     */
+    public void writeToken(String token) throws IOException {
+        if (token == null || token.isEmpty()){
+            write(separator);
+        } else {
+            token = escape(token);
+            write(token + separator);
+        }
+    }
+
+    /**
+     * Escape any CSV separators by surrounding the token in double quotes
+     * @param token String token to be written to CSV
+     * @return Escaped CSV token
+     */
+    @NonNull
+    private String escape(@NonNull String token) {
+        if (token.contains(separator)){
+            return "\"" + token + "\"";
+        }
+        return token;
+    }
+
+    /**
+     * Writes a token to the CSV file and appends end of line to it.
+     *
+     * The token **MUST NOT** not contain the CSV separator. If the separator is found in the token, then
+     * the token will be escaped as specified by RFC 4180
+     * @param token The token to be written to the file
+     * @throws IOException if token could not be written to underlying writer
+     */
+    public void writeEndToken(String token) throws IOException {
+        if (token != null && !token.isEmpty()) {
+            write(escape(token));
+        }
+        this.newLine();
+    }
+
 }
