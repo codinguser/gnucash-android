7a4e4e5b763429dfad6b9dd1b2899a77c98123a2
JeanGarf <thierryw777@gmail.com>
2020-02-27 17:50:06 +0100

    Fixes #872 Add a final commit

--- app/src/main/java/org/gnucash/android/ui/report/ReportType.java
+++ app/src/main/java/org/gnucash/android/ui/report/ReportType.java
@@ -39,6 +39,7 @@ import java.util.Map;
 public enum ReportType {
     PIE_CHART(0), BAR_CHART(1), LINE_CHART(2), TEXT(3), NONE(4);
 
+    // #872 Use a list to be sure of the sort order which is not guaranted with a hashmap keys
     List<String> mReportNames = null;
 
     Map<String, Class> mReportTypeMap = new HashMap<>();
@@ -56,29 +57,16 @@ public enum ReportType {
 
         Context context = GnuCashApplication.getAppContext();
 
-//        switch (index){
-//            case 0:
-//                mReportTypeMap.put(context.getString(R.string.title_pie_chart), PieChartFragment.class);
-//                break;
-//            case 1:
-//                mReportTypeMap.put(context.getString(R.string.title_bar_chart), StackedBarChartFragment.class);
-//                break;
-//            case 2:
-//                mReportTypeMap.put(context.getString(R.string.title_cash_flow_report), CashFlowLineChartFragment.class);
-//                break;
-//            case 3:
-//                mReportTypeMap.put(context.getString(R.string.title_balance_sheet_report), BalanceSheetFragment.class);
-//                break;
-//            case 4:
-//                break;
-//        }
-
+        // #872 Fill the map with all the items, in order to fill the Report Toolbar Spinner
         mReportTypeMap.put(context.getString(R.string.title_pie_chart),
                            PieChartFragment.class);
+
         mReportTypeMap.put(context.getString(R.string.title_bar_chart),
                            StackedBarChartFragment.class);
+
         mReportTypeMap.put(context.getString(R.string.title_cash_flow_report),
                            CashFlowLineChartFragment.class);
+
         mReportTypeMap.put(context.getString(R.string.title_balance_sheet_report),
                            BalanceSheetFragment.class);
     }
